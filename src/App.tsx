fad5991b26b0cdeb3a08e34ac1cad004c228183e
Sergey Lipikhin <football3@ukr.net>
2023-10-12 02:59:18 +0300

    Start with arcGIS and reader map

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import esriConfig from '@arcgis/core/config';
+import MapView from '@arcgis/core/views/MapView';
+import Map from '@arcgis/core/Map';
+import './App.css';
 
 export const App: React.FC = () => {
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    // eslint-disable-next-line max-len
+    esriConfig.apiKey = 'AAPK4b4113c9c3744936bfb62773e5264aeephEk2ardc21L5QjW12x19tQAoumUJIo3wOlYStHpMuhNzZTmf7cuv7okg-D0BUPY';
+
+    const map = new Map({
+      basemap: 'osm-standard',
+    });
+
+    const view = new MapView({
+      map,
+      container: mapRef.current ?? undefined,
+      center: [-112, 38],
+      zoom: 13,
+    });
+
+    return () => view.destroy();
+  }, []);
+
   return (
-    <p className="fs-1">Hello</p>
+    <>
+      <div ref={mapRef} className="map-view" />
+    </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { App } from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
+import './styles/index.css';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <App />,
 );
